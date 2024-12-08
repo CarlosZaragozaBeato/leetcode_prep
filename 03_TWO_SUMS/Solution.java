450b5317e9f86314860bbd07ade9b212ceff04be
Carlos <carloszaragozabeato15@gmail.com>
2024-12-08 09:46:11 +0100

    2024.12.08

--- 03_TWO_SUMS/Solution.java
+++ 03_TWO_SUMS/Solution.java
@@ -1,16 +1,93 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     /*
-     *  Given an array of integers nums and an integers target, return the indices 
-     *  i and j such that nums[i] + nums [j] == target and i != j
+     * Given an array of integers nums and an integers target, return the indices
+     * i and j such that nums[i] + nums [j] == target and i != j
      * 
      * 
-     * You may assume that every input has exactly one pair of indices 
+     * You may assume that every input has exactly one pair of indices
      * i and j that sastify the condition
      */
-        
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (seen.containsKey(diff)) {
+                return new int[] {
+                        seen.get(nums[i]), i
+                };
+            }
+
+            seen.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+
+
+    // BRUTE FORCE 
+    public int[] bruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j<nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+
+    // Sorting 
+    public int[] sorting(int[] nums, int target){
+        int[][] A = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            A[i][0] = nums[i];
+            A[i][1] = i;
+        }
+        Arrays.sort(A, Comparator.comparingInt(a -> a[0]));
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int cur = A[i][0] + A[j][0];
+            if (cur == target) {
+                return new int[]{Math.min(A[i][1], A[j][1]), 
+                                 Math.max(A[i][1], A[j][1])};
+
+            }else if (cur < target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return new int[0];
+    }
+
+    // HashMap Two pass
+    public int[] hashMapTwoPass(int[] nums, int target) {
+        Map<Integer, Integer> indices = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            indices.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if (indices.containsKey(diff) && indices.get(diff) != i){
+                return new int[]{
+                    i, indices.get(diff)
+                };
+            }
+        }
+        return new int[0];
+    }
+
 
     public static void main(String[] args) {
-        
+
     }
 }
--- 03_TWO_SUMS/Solution.py
+++ 03_TWO_SUMS/Solution.py
@@ -1,14 +1,66 @@
-
+from typing import List 
 
 
 class Soluction:
+    """
+        Given an array of integers nums and a integer target, return the indices i and j 
+        such that nums[i] == target and i != j
 
-    def two_sums(self):
-        pass 
+        You may assume that every input has exactly one pair of indices i and j that satisfy 
+        the condition
+    """
+
+
+    def two_sums(self, nums:List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num 
+            if diff in seen:
+                return [seen[i], i]
+            seen[num] = i  
+        return []
+    
+
+    # Brute Force 
+    def brute_force(self, nums:List[int], target:int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+    # Sorting 
+    def sorting(self, nums:List[int], target:int) -> List[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i])        
+        A.sort()
+        i, j = 0, len(nums) -1
+        while i < j:
+            cur = A[i][0] + A[j][0]
+            if cur == target:
+                return [min(A[i][1], A[j][1]),
+                        max(A[i][1], A[j][1])]
+            elif cur < target:
+                i += 1
+            else:
+                j -= 1
+        return []
+    
+    # HashMap Two Passes 
+    def hash_map_two_passes(self, nums:List[int], target:int)-> List[int]:
+        indices = {}
+        for i,n in enumerate(nums):
+            indices[n] = i
+
+        for i,n in enumerate(nums):
+            diff = target - n 
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
 
 
 
-    def soluction01():
-        pass 
 
         
