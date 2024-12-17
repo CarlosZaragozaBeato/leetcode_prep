ed44a955331fe4d46c043d96523966184d55618d
carlos <carloszaragozabeato15@gmail.com>
2024-12-17 16:41:27 +0100

    2024.12.17

---
+++ stack/06_car_fleet/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+    public int stack(int target, int[] position, int[] speed){
+        int[][] pair = new int[position.length][2];
+        for (int i = 0; i <position.length; i++){
+            pair[i][0] = position[i];
+            pair[i][1] = speed
+        }
+    }
+}
