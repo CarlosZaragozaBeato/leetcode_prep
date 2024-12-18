b73d07540d487f2e00aaf8b2b1bc5f3fcd00412c
Carlos <carloszaragozabeato15@gmail.com>
2024-12-18 11:32:47 +0100

    2024.12.18

---
+++ binary_search/07_median_of_two_sorted_arrays/main.py
@@ -0,0 +1,93 @@
+from typing import List 
+
+def bruteForce(nums1:List[int], nums2:List[int]) -> float:
+        len1 = len(nums1)
+        len2 = len(nums2)
+        merged = nums1 + nums2
+        merged.sort()
+        
+        totalLen = len(merged)
+        if totalLen % 2 == 0:
+            return (merged[totalLen // 2 - 1] + merged[totalLen // 2]) / 2.0
+        else:
+            return merged[totalLen // 2]
+        
+
+def twoPointers(nums1:List[int], nums2:List[int]) -> float:
+        len1, len2 = len(nums1), len(nums2)
+        i = j = 0
+        median1 = median2 = 0
+
+        for count in range((len1 + len2) // 2 + 1):
+            median2 = median1
+            if i < len1 and j < len2:
+                if nums1[i] > nums2[j]:
+                    median1 = nums2[j]
+                    j += 1
+                else:
+                    median1 = nums1[i]
+                    i += 1
+            elif i < len1:
+                median1 = nums1[i]
+                i += 1
+            else:
+                median1 = nums2[j]
+                j += 1
+
+        if (len1 + len2) % 2 == 1:
+            return float(median1)
+        else:
+            return (median1 + median2) / 2.0
+
+
+
+def get_kth( a: List[int], m: int, b: List[int], n: int, k: int, a_start: int = 0, b_start: int = 0) -> int:
+        if m > n:
+            return get_kth(b, n, a, m, k, b_start, a_start)
+        if m == 0:
+            return b[b_start + k - 1]
+        if k == 1:
+            return min(a[a_start], b[b_start])
+        
+        i = min(m, k // 2)
+        j = min(n, k // 2)
+        
+        if a[a_start + i - 1] > b[b_start + j - 1]:
+            return get_kth(a, m, b, n - j, k - j, a_start, b_start + j)
+        else:
+            return get_kth(a, m - i, b, n, k - i, a_start + i, b_start)    
+def binarySearch(nums1: List[int], nums2: List[int]) -> float:
+        left = (len(nums1) + len(nums2) + 1) // 2
+        right = (len(nums1) + len(nums2) + 2) // 2
+        return (get_kth(nums1, len(nums1), nums2, len(nums2), left) +
+                get_kth(nums1, len(nums1), nums2, len(nums2), right)) / 2.0
+
+
+
+def binarySearchOptimal(nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        if len(B) < len(A):
+            A, B = B, A
+
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
+
