3b20e7e945aff342e0aeda2d84845b1c57951c42
Carlos <carloszaragozabeato15@gmail.com>
2024-12-18 08:32:22 +0100

    2024.12.18

---
+++ binary_search/03_eating_bananas/main.py
@@ -0,0 +1,29 @@
+from typing import List 
+import math 
+def bruteForce(piles:List[int], h:int) -> int:
+    speed = 1
+    while True:
+        totalTime = 0
+        for pile in piles:
+            totalTime += math.ceil(pile / speed)
+        if totalTime <= h:
+            return speed 
+        speed += 1
+    
+    return speed
+
+
+def binarySearch(piles:List[int], h:int)->int:
+    l, r = 1, max(piles)
+    res = r 
+    while l <= r:
+        k = (l + r)//2
+        totalTime = 0
+        for p in piles:
+            totalTime += math.ceil(float(p) / k)
+        if totalTime <= h:
+            res = k 
+            r = k -1 
+        else:
+            l = k +1
+    return res 
